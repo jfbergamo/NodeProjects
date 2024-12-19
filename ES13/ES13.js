dcc17c8e933f95ea1fb425578528119707dd2ec0
J <jacopobergamasco06@gmail.com>
2024-12-19 12:01:18 +0100

    Implementazione iniziale punto f (non ancora funzionante)

--- ES13/ES13.js
+++ ES13/ES13.js
@@ -1,6 +1,7 @@
 // Bergamasco Jacopo, 5AIA, A.S. 2024-2025
 
 const net = require('net');
+const { disconnect } = require('process');
 
 const PORT = 7979;
 const dev = 'Bergamasco Jacopo';
@@ -9,10 +10,16 @@ const endl = '\r\n';
 const server = net.createServer((socket) => {
     // Init
     server.sockets.push(socket);
-    socket.IP = (() => {
-        const addr = socket.remoteAddress;
-        return [(addr === socket.localAddress ? 'localhost' : addr.toString()), socket.remotePort];
-    })();
+    socket.clientAddr = {
+        'ip': (() => {
+            const addr = socket.remoteAddress;
+            return (addr === socket.localAddress ? 'localhost' : addr.toString());
+        })(),
+        'port': socket.remotePort,
+    };
+    socket.name = () => {
+        return `${socket.clientAddr.ip}:${socket.clientAddr.port}`;
+    };
     socket.loginTime = new Date();
     socket.write(`${dev}${endl}> `)
     let input = '';
@@ -21,7 +28,7 @@ const server = net.createServer((socket) => {
     
     server.on('broadcast', (msg) => {
         server.sockets.forEach((sock) => {
-            sock.write(`${msg}${endl}> `);
+            sock.write(`[${socket.name()}] ${msg}${endl}> `);
         });
     });
     
@@ -40,11 +47,18 @@ const server = net.createServer((socket) => {
                 case 'time':
                     socket.write(`Tempo trascorso: ${(new Date().getTime() - socket.loginTime.getTime())/1000}s${endl}> `);
                     break;
+                case 'quit':
+                    if (args.length != 2) {
+                        socket.write(`[ERRORE] Sintassi: quit <ip> <port>`);
+                    } else {
+
+                    }
+                    break;
                 case 'exit':
                     socket.emit('disconnect');
                     return;
                 default:
-                    console.log(`[${socket.IP}] ${input}`)
+                    console.log(`[${socket.name()}] ${input}`)
                     socket.write(`${input}${endl}> `);
                     break;
                 }
@@ -52,23 +66,31 @@ const server = net.createServer((socket) => {
         } else {
             input += data;
         }
-        
     });
     
     socket.on('disconnect', () => {
         server.sockets.splice(server.sockets.indexOf(socket), 1);
-        const message = `Disconnessione [${socket.IP}] in corso. Client connessi: ${server.sockets.length}.`;
+        const message = `Disconnessione [${socket.name()}] in corso. Client connessi: ${server.sockets.length}.`;
         socket.write(message);
         socket.end();
         console.log(message);
-    })
+    });
+
+    server.on('kick', (addr) => {
+        server.sockets.forEach((sock) => {
+            if (sock.clientAddr.ip === addr.ip && sock.clientAddr.port === addr.port) {
+                sock.emit('disconnect');
+                return;
+            }
+        })
+    });
 });
 
 server.sockets = [];
 server.getNames = () => {
     const names = [];
     server.sockets.forEach((sock) => {
-        names.push(`[${sock.IP}]`);
+        names.push(`[${sock.clientAddr.ip}:${sock.clientAddr.port}]`);
     });
     return names;
 };
